ca5379fd51c34fa88f3d96f93f327c23776e06c9
Petar Mihaylovski <459725@Student.saxion.nl>
2021-10-12 18:16:26 +0200

    remapping the api response

--- src/store/shop.js
+++ src/store/shop.js
@@ -17,7 +17,7 @@ class ShopStore {
     }
 
     async getShopsFromAPI() {
-        await axios.get('http://10.0.2.2:8000/api/shop', {
+        await axios.get('http://10.0.2.2:8000/api/shops', {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Accept': 'application/json'
@@ -26,7 +26,16 @@ class ShopStore {
             .then((response) => {
                 // handle success
                 if (response.status === 200) {
-                    shopStore.shops = response.data;
+                    shopStore.shops = response.data.map((shop) => {
+                        return {
+                            id: shop.id,
+                            name: shop.name,
+                            description: shop.description,
+                            products: [],
+                            imageURI: shop.image_url,
+                            createdAt: shop.created_at
+                        };
+                    });
                 }
             })
             .catch((error) => {
