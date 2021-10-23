3ea07e15559c8256f7ed19da889efaddfa43ceaf
petar <459725@student.saxion.nl>
2021-10-23 13:16:46 +0200

    carousel added to product view as well

--- src/components/view_components/ProductCard/index.js
+++ src/components/view_components/ProductCard/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import {Text, View, Image} from "react-native";
 import styles from "./styles";
+import Carousel from "react-native-snap-carousel";
 
 const ProductCard = ({product}) => {
     return (
         <View style={styles.container}>
-            <Image
-                style={styles.image}
-                source={{uri: product.imageURI}}/>
+            <Carousel
+                data={product.images}
+                renderItem={({item}) => (
+                    <Image style={styles.image}
+                           source={{uri: item}}
+                    />
+                )}
+                sliderWidth={350}
+                itemWidth={350}
+                loop
+            />
             <View style={styles.textContainer}>
                 <Text style={styles.name}>{product.name}</Text>
                 <Text style={styles.price}>{product.price}</Text>
