0b37d06fe8f402e3e60d8dc9bed58c326fcbe376
petar <459725@student.saxion.nl>
2021-10-24 14:26:11 +0200

    calculating the price of an ordered product

--- src/components/create_components/ProductPreviewCard/index.js
+++ src/components/create_components/ProductPreviewCard/index.js
@@ -5,7 +5,7 @@ import Carousel from "react-native-snap-carousel";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import Entypo from "react-native-vector-icons/Entypo";
 
-const ProductPreviewCard = ({product, count=0 ,controlButtons}) => {
+const ProductPreviewCard = ({product, count = 0, controlButtons}) => {
 
     return (
         <View style={styles.container}>
@@ -25,9 +25,10 @@ const ProductPreviewCard = ({product, count=0 ,controlButtons}) => {
             </View>
             <View style={styles.textContainer}>
                 <Text style={styles.name} numOfLines={2}>{product.name}</Text>
-                <Text style={styles.info}>Price: {'\u20AC'} {product.price}</Text>
-                <Text style={styles.info}>Stock Availability: {product.stock}</Text>
+                <Text style={[styles.info, {alignSelf:'flex-start'}]}>Price: {'\u20AC'} {product.price}</Text>
+                {controlButtons && <Text style={styles.info}>Stock Availability: {product.stock}</Text>}
                 <Text style={styles.info}>Category: {product.category}</Text>
+                {!controlButtons && <Text style={styles.info}>Total: {'\u20AC'} {(Number(product.price) * count).toFixed(2)}</Text>}
             </View>
 
             {controlButtons
@@ -51,7 +52,7 @@ const ProductPreviewCard = ({product, count=0 ,controlButtons}) => {
                         style={styles.button}>
                         <Text style={{fontSize: 16, color: '#474747'}}>+</Text>
                     </Pressable>
-                    <Text style={{marginLeft:7,fontSize: 16}}>{count}</Text>
+                    <Text style={{marginLeft: 7, fontSize: 16}}>{count}</Text>
                     <Pressable
                         onPress={() => {
                         }}
