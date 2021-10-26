88e46cfb9ecc76b9b8b323b3121095442896228f
petar <459725@student.saxion.nl>
2021-10-26 20:41:29 +0200

    auth compnents in-code docs

--- src/components/auth/ForgotPasswordComponent/index.js
+++ src/components/auth/ForgotPasswordComponent/index.js
@@ -2,6 +2,9 @@ import {Pressable, Text, View} from "react-native";
 import React from "react";
 import styles from "./styles";
 
+/**
+ * Stateless component used to display the forgot your password button
+ */
 const ForgotPassword = () => {
     return (
         <View style={styles.forgotContainer}>
--- src/components/auth/RememberMe/index.js
+++ src/components/auth/RememberMe/index.js
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
 import CheckBox from "@react-native-community/checkbox";
 import {Text, View} from "react-native";
 import styles from './styles';
 
+/**
+ * Stateless component that represents the remember me checkbox
+ * @param storeSession initial checkbox value
+ * @param handler whenever value changes
+s */
 const RememberMeCheckbox = ({storeSession, handler }) => {
-
     return (
         <View style={styles.row}>
             <CheckBox
